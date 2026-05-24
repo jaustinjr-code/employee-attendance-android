ebda24350440521c934938b760f0b2835d85e205
jaustinjr-code <jaustinjr.collab@gmail.com>
2026-05-23 18:48:00 -0700

    Fix warnings

--- EmployeeAttendance/app/src/main/java/com/jaustinjr/employeeattendance/ui/attendance/AttendanceScreen.kt
+++ EmployeeAttendance/app/src/main/java/com/jaustinjr/employeeattendance/ui/attendance/AttendanceScreen.kt
@@ -1,18 +1,8 @@
 package com.jaustinjr.employeeattendance.ui.attendance
 
-import androidx.fragment.app.viewModels
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.jaustinjr.employeeattendance.ui.theme.EmployeeAttendanceTheme
