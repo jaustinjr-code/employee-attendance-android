e9a6a59af11934a682b3f09d86557047616ce687
jaustinjr-code <jaustinjr.collab@gmail.com>
2026-05-23 20:15:44 -0700

    Add top app bar

--- EmployeeAttendance/app/src/main/java/com/jaustinjr/employeeattendance/MainActivity.kt
+++ EmployeeAttendance/app/src/main/java/com/jaustinjr/employeeattendance/MainActivity.kt
@@ -4,9 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
 import com.jaustinjr.employeeattendance.ui.attendance.AttendanceScreen
 import com.jaustinjr.employeeattendance.ui.theme.EmployeeAttendanceTheme
 import kotlinx.serialization.Serializable
@@ -15,15 +29,28 @@ import kotlinx.serialization.Serializable
 object Attendance
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             EmployeeAttendanceTheme {
                 val navController = rememberNavController()
-                // Host your navigation graph here
-                NavHost(navController, startDestination = Attendance) {
-                    composable<Attendance> { AttendanceScreen() }
+                var appBarTitle by remember { mutableStateOf("Attendance") }
+
+                Scaffold(
+                    topBar = {
+                        TopAppBar(title = {
+                            Text(appBarTitle, maxLines = 1, overflow = TextOverflow.Ellipsis)
+                        })
+                    }
+                ) { padding ->
+                    NavHost(navController, startDestination = Attendance, modifier = Modifier.padding(padding)) {
+                        composable<Attendance> {
+                            appBarTitle = "Attendance"
+                            AttendanceScreen()
+                        }
+                    }
                 }
             }
         }
