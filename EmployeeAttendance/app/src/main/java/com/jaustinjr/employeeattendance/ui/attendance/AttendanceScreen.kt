c85cfb566cc9886cfe4bc24c77c548db95f01197
jaustinjr-code <jaustinjr.collab@gmail.com>
2026-05-25 11:22:56 -0700

    Add clock
    Time check component with clock reference https://stackoverflow.com/questions/71191340/how-can-i-implement-a-timer-in-a-portable-way-in-jetpack-compose

--- EmployeeAttendance/app/src/main/java/com/jaustinjr/employeeattendance/ui/attendance/AttendanceScreen.kt
+++ EmployeeAttendance/app/src/main/java/com/jaustinjr/employeeattendance/ui/attendance/AttendanceScreen.kt
@@ -1,10 +1,24 @@
 package com.jaustinjr.employeeattendance.ui.attendance
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInteropFilter
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -12,6 +26,10 @@ import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.jaustinjr.employeeattendance.ui.theme.EmployeeAttendanceTheme
+import kotlinx.coroutines.delay
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 @Composable
 fun AttendanceScreen(
@@ -27,7 +45,7 @@ fun AttendanceScreen(
 ) {
     Column(modifier = modifier) {
         Greeting(todayDate = todayDate, modifier = Modifier.padding(20.dp))
-        // TODO clock in and out component
+        TimeCheck(modifier = Modifier.padding(20.dp))
         // TODO location bubble component
     }
 }
@@ -51,4 +69,41 @@ fun GreetingPreview() {
             modifier = Modifier.padding(20.dp)
         )
     }
+}
+
+@Composable
+fun TimeCheck(
+    modifier: Modifier = Modifier
+) {
+    ElevatedCard(modifier = modifier.fillMaxWidth().height(300.dp)) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxSize().padding(20.dp)) {
+            Text("Current Time")
+            LiveClock()
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TimeCheckPreview() {
+    EmployeeAttendanceTheme {
+        TimeCheck(
+            modifier = Modifier.padding(20.dp)
+        )
+    }
+}
+
+@Composable
+fun LiveClock() {
+    var timeText by remember { mutableStateOf("") }
+
+    LaunchedEffect(Unit) {
+        while (true) {
+            val now = Calendar.getInstance().time
+            timeText = SimpleDateFormat("HH:mm:ss", Locale.US).format(now)
+            delay(1000) // Update every secondary
+        }
+    }
+
+    Text(text = "Live Time: $timeText")
 }
