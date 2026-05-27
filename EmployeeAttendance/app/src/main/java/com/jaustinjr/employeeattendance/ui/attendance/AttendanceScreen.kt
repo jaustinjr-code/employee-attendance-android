29a433a361ae99f91edbfabb20ce90549ac6bdaa
jaustinjr-code <jaustinjr.collab@gmail.com>
2026-05-26 17:43:10 -0700

    Add location chip and icons
    Includes location unknown, off, and on icons.
    TODO location on and off states. Add location permission dialog. Location permission manifest settings. Location state changes.
    TODO start extracting logic to view model or business layer.

--- EmployeeAttendance/app/src/main/java/com/jaustinjr/employeeattendance/ui/attendance/AttendanceScreen.kt
+++ EmployeeAttendance/app/src/main/java/com/jaustinjr/employeeattendance/ui/attendance/AttendanceScreen.kt
@@ -7,10 +7,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AssistChipDefaults
 import androidx.compose.material3.Button
+import androidx.compose.material3.ElevatedAssistChip
 import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -21,12 +25,14 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.pointerInteropFilter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.jaustinjr.employeeattendance.R
 import com.jaustinjr.employeeattendance.ui.theme.EmployeeAttendanceTheme
 import kotlinx.coroutines.delay
 import java.text.SimpleDateFormat
@@ -48,7 +54,7 @@ fun AttendanceScreen(
     Column(modifier = modifier) {
         Greeting(todayDate = todayDate, modifier = Modifier.padding(20.dp))
         TimeCheck(modifier = Modifier.padding(20.dp))
-        // TODO location bubble component
+        LocationAssist(Modifier.padding(20.dp).align(alignment = Alignment.CenterHorizontally))
     }
 }
 
@@ -129,4 +135,26 @@ fun LiveClock(modifier: Modifier = Modifier) {
     }
 
     Text(text = timeText, fontWeight = FontWeight.Bold, fontSize = 10.em, modifier = modifier)
+}
+
+@Composable
+fun LocationAssist(modifier: Modifier = Modifier) {
+    ElevatedAssistChip(
+        onClick = { /* TODO open location permission dialog */ },
+        label = { Text("Setup Location") },
+        leadingIcon = {
+            Icon(
+                painter = painterResource(R.drawable.not_listed_location_24px),
+                contentDescription = "Localized description",
+                modifier = Modifier.size(AssistChipDefaults.IconSize),
+            )
+        },
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LocationAssistPreview() {
+    LocationAssist()
 }
